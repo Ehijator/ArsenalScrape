4dac9d7ea46c429f80e4b11bae6e399503547467
Ehijator <ehijatora@gmail.com>
2022-07-17 12:40:02 +0100

    fleshed out nav function

--- ArsenalScrape.py
+++ ArsenalScrape.py
@@ -10,20 +10,41 @@ import pandas
 chrome_options = Options()
 S = Service("C:\Python310\chromedriver.exe")
 #chrome_options.add_argument("--headless")
-chrome_options.add_argument("--s")
+chrome_options.add_argument("--start-maximized")
 driver = webdriver.Chrome(service= S, options= chrome_options)
 starturl = r"https://fbref.com/en/squads/18bb7c10/history/Arsenal-Stats-and-History#all_comps_fa_club_league"
 
 driver.get(starturl)
-season = []
-def Nav():
-    time.sleep(5)
-    driver.find_element(By.XPATH,'//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]').click()
-    vals = driver.find_elements(By.XPATH,"//*[@id='comps_fa_club_league']/tbody/tr/*[@_data-stat = 'season']" )
-    for s in range(len(vals)):
-        season.append()
 
-    print(season)
+def Nav():
+    time.sleep(10)
+    driver.find_element(By.XPATH,'//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]').click()
+    dg = driver.find_elements(By.XPATH,"//*[@id='comps_fa_club_league']/tbody/tr")
+    for tr in dg:
+        Season = tr.find_element(By.XPATH,"./th[1]").text
+        Competition = tr.find_element(By.XPATH,"./td[3]/a").text
+        LeagueRank = tr.find_element(By.XPATH,"./td[4]").text
+        Games =
+        Wins=
+        Draws=
+        Losses=
+        GoalsFor=
+        GoalsAgainst=
+        GoalDiff=
+        Points=
+        AttendancePerG=
+
+        print(Season,Competition,LeagueRank)
+
+#//*[@id="comps_fa_club_league"]/tbody/tr[1]/td[5]
+
+#dg = driver.find_elements(By.XPATH,"//*[@id='comps_fa_club_league']/tbody/tr/th" )   
+#     for tr in dg:
+#         print(tr.text)
+#         season.append(tr.text)
+# print(season)
+    
+    
     
 
 Nav()
